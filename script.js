84b378503b3ce7005c7a4eca4d8d2d87c79bd99f
IT21314742 <it21314742@my.sliit.lk>
2024-05-16 10:07:21 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ var emailField = document.getElementById("email-field");
 var emailLabel = document.getElementById("email-label");
 var emailError = document.getElementById("email-error");
 
-function validateEmail
+function validateEmail()
