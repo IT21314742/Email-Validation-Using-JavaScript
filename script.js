8c076445c19fb4ff251cf681181360155ee2652e
IT21314742 <it21314742@my.sliit.lk>
2024-05-16 10:07:31 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ var emailField = document.getElementById("email-field");
 var emailLabel = document.getElementById("email-label");
 var emailError = document.getElementById("email-error");
 
-function validateEmail(){}
+function validateEmail(){
+    
+}
