f435440277c3cdfaea649be44d0f29d98c921be2
IT21314742 <it21314742@my.sliit.lk>
2024-05-16 10:30:46 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ var emailError = document.getElementById("email-error");
 function validateEmail(){
     emailLabel.style.bottom = "45px";
 
-    //Improved email validation
+    //Improved email validation using
 }
