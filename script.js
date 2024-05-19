3e842021ac8361c8a06fbe7202ec4343cbc71dc4
IT21314742 <it21314742@my.sliit.lk>
2024-05-19 09:58:16 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function validateEmail() {
     if (!emailField.value.match(emailPattern)) {
         emailError.innerHTML = "Please Enter a valid Email";
         emailField.style.borderBottomColor = "red";
-        emailError.style.top = "05%";
+        emailError.style.top = "00%";
         return false;
     }
     emailError.innerHTML = "";
